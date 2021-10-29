541395666236ed663f0e678d84e3a2dc61d3ce37
root <root@DESKTOP-DHUR55B.localdomain>
2021-10-29 18:39:26 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql'); 
+const app = express();
+const port =  3000;
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) VALUES ('Matheus')`;
+connection.query(sql);
+
+app.get('/', (req, res) => {
+  const selectQuery = `SELECT name FROM people`;
+  var namesList = '';
+  connection.query(selectQuery, (err, result, fields) => {
+    if (err) throw err;
+    Object.keys(result).forEach((key) => {
+      var row = result[key];
+      namesList+=`<li>${row.name}</li>`
+    })
+    res.send(`<h1>Full Cycle Rocks!</h1><lu>${namesList}</lu>`);
+  });
+});
+
+app.listen(port, () => {
+  console.log('Rodando na porta ' + port);
+})
