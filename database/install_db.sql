e6aa78e98b391f7cd53da3f70ef66c17e85b9a57
gomesmatheus <vsgmatheus@gmail.com>
2021-11-03 20:17:56 -0300

    agora vai

--- database/install_db.sql
+++ database/install_db.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE nodedb;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 USE nodedb;
 CREATE TABLE IF NOT EXISTS people (id integer auto_increment, name VARCHAR(255), PRIMARY KEY  (id));
